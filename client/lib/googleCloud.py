71b45281c81c71f442a00669821d4a0a4ac0a7a4
pottava <pottava@gmail.com>
2024-04-23 00:33:09 +0900

    Revise tag & code

--- client/lib/googleCloud.py
+++ client/lib/googleCloud.py
@@ -1,15 +1,5 @@
-import logging
-import sys
-
 import google.auth
 import google.auth.transport.requests
-
-logging.basicConfig(
-    stream=sys.stdout,
-    level=logging.DEBUG,
-    format="%(asctime)s, %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
 
 # API 接続先
 API_ENDPOINT = "https://app-xxxxx-an.a.run.app"
