7e8c0092693df112ade90fe8cbddf9db72a046f3
pottava <pottava@gmail.com>
2024-04-23 12:06:29 +0900

    Update tags with its githash

--- server/src/versions.js
+++ server/src/versions.js
@@ -4,10 +4,11 @@ router.disable("x-powered-by");
 
 router.get("/", async (req, res) => {
   const projectId = process.env.PROJECT_ID || "unknown";
+  const latestTag = process.env.CURRENT_VERSION || "latest";
 
   res.json({
-    client_001: `asia-northeast1-docker.pkg.dev/${projectId}/apps/client-001:71b4528`,
-    client_002: `asia-northeast1-docker.pkg.dev/${projectId}/apps/client-002:71b4528`,
+    client_001: `asia-northeast1-docker.pkg.dev/${projectId}/apps/client-001:${latestTag}`,
+    client_002: `asia-northeast1-docker.pkg.dev/${projectId}/apps/client-002:${latestTag}`,
   });
 });
 
